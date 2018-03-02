4ac9565e50c9462f32cd70dbfd0578b2546b1f96
CM Student <cmstudent@uhwo-acm-c-14.local>
2018-03-01 15:55:01 -1000

    Completed Hackwich #5

--- HackwichFive/ViewController.swift
+++ HackwichFive/ViewController.swift
@@ -8,8 +8,11 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewControllerThree: UIViewController, UITableViewDataSource, UITableViewDelegate {
+    
+    //myFriendsArray
 
+    @IBOutlet weak var tableView: UITableView!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -19,7 +22,17 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1;
+    }
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return myFriendsArray.count
+    }
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath)->UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cellReuseIdentifier")!
+        let text = myFriendsArray[indexPath.row]
+        cell.textLabel?.text = text
+        return cell
 }
 
+}
